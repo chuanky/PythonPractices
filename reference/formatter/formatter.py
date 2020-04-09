605a02c1165f4e21e0c356f9423ba3c4ba6e07f4
chuanky <shicq01@gmail.com>
2020-04-09 23:44:21 +0800

    Update formatter

--- reference/formatter/formatter.py
+++ reference/formatter/formatter.py
@@ -1,3 +1,9 @@
-text = 'APPROPRIATE PROBLEMS FOR NEURAL NETWORK LEARNING'
+text = ''
 
-print(text.lower().title())
+while True:
+    if text == 'quit':
+        break
+    text = input("输入想要转换的字符: ")
+    print(text.lower().title())
+
+
